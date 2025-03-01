4deae8c638e180db32b4d06391711caacd0240a3
Doduy2110 <duydvph46536@fpt.edu.vn>
2025-03-02 01:43:32 +0700

    fig lỗi font awe và cắt giao diện admin

--- app/Http/Controllers/Backend/DashboardController.php
+++ app/Http/Controllers/Backend/DashboardController.php
@@ -7,11 +7,11 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    public function __construct(){
-        
-    }
+    public function __construct() {}
 
-    public function index(){
-        return view('backend.dashboard.index');
+    public function index()
+    {
+        $template = 'backend.dashboard.home.index';
+        return view('backend.dashboard.layout', compact('template'));
     }
 }
